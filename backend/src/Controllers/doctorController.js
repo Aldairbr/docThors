dae1b2c3be0349bb466eae3ae6cc3dc553993d68
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-12-29 20:11:51 -0300

    teste de mudança para merge

--- backend/src/Controllers/doctorController.js
+++ backend/src/Controllers/doctorController.js
@@ -14,6 +14,7 @@ import {
 
 const doctorController = {
   store: async (request, response) => {
+    //mudança para testar merge
     const { body } = request;
 
     const doctorSchema = Yup.object().shape({
